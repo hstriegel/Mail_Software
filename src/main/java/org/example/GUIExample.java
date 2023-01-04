b4f38baf50237474c1e8258c73a117547a4c23c7
Hunter <hstriege@purdue.edu>
2023-01-04 03:29:22 -0500

    Some Menu Bar stuff

---
+++ src/main/java/org/example/GUIExample.java
@@ -0,0 +1,38 @@
+package org.example;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class GUIExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Emails");
+        JLabel l1 = new JLabel("1");
+        l1.setHorizontalAlignment(JLabel.CENTER);
+        Container pane = frame.getContentPane();
+        pane.add(l1, BorderLayout.CENTER);
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.add(new Button("Button1"));
+        p.add(new Button("Button2"));
+        p.add(new Button("Button3"));
+        p.add(new Button("Button4"));
+        p.add(new Button("Button5"));
+        p.add(new Button("Button6"));
+
+        JMenuBar menu = new JMenuBar();
+        JMenu m = new JMenu("Menu");
+        menu.add(m);
+        frame.setJMenuBar(menu);
+        JMenuItem mi = new JMenuItem("Item");
+        menu.add(mi);
+
+
+        JScrollPane scrollablebuttons = new JScrollPane(p);
+        scrollablebuttons.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollablebuttons.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        pane.add(scrollablebuttons, BorderLayout.WEST);
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
